5bed5823dbb2e49c55fc6051ca55e46ea70f3253
Diego Vilhalva <diegorocha450@yahoo.com>
2025-05-16 11:01:22 -0300

    making the project responsive

--- src/components/FrequentTraveler.tsx
+++ src/components/FrequentTraveler.tsx
@@ -66,21 +66,21 @@ const FrequentTraveler = () => {
     }
 
     return (
-        <section className="bg-primary-100 px-24 py-36">
-            <div className="border-y-grey-500/40 m-auto flex max-w-389 items-center justify-between gap-x-28 border-y-1 py-26">
-                <div className="basis-150 text-center">
-                    <h3 className="tracking-6 mb-9.5 text-[1.75rem]/14 font-semibold">Learn About Our frequent Travaler Program</h3>
+        <section className="bg-primary-100 max-3xl:px-20 max-3xl:py-34 px-24 py-36 max-2xl:px-14 max-2xl:py-28 max-xl:px-10 max-xl:py-26 max-lg:px-6 max-lg:pt-24 max-lg:pb-32 max-md:pb-28 max-sm:px-4">
+            <div className="border-y-grey-500/40 m-auto flex max-w-389 items-center justify-between gap-x-28 border-y-1 py-26 max-2xl:gap-x-20 max-2xl:py-20 max-xl:gap-x-10 max-xl:py-16 max-lg:max-w-lg max-lg:flex-col max-lg:gap-y-16 max-lg:border-none max-lg:py-0">
+                <div className="basis-150 text-center max-lg:basis-auto">
+                    <h3 className="tracking-6 max-3xl:text-[1.5rem]/12 max-3xl:mb-8 mb-9.5 text-[1.75rem]/14 font-semibold max-md:mb-9.5">Learn About Our frequent Travaler Program</h3>
                     <p className="text-grey-800 text-base/13.5">
                         Interested in saving up to $1000 on your next vacation? How about
                         earning travel points that can be converted into rewards like extra
                         nights, free meals, and exclusive offers from resorts around globe?
                     </p>
                 </div>
-                <div className="bg-grey-500/40 block w-0.25 self-stretch" />
-                <form className="flex flex-col basis-150">
-                    <label className="mb-8">
-                        <p className="tracking-6 mb-3 text-lg/9.5 font-semibold">Full Name</p>
-                        <input type="text" name="fullName" value={values.fullName} onChange={handleChange} minLength={2} maxLength={50} disabled={formState.currentState !== "idle"}  placeholder="John Doe" className={`placeholder:text-grey-400 w-full rounded-lg bg-white py-3.5 pl-4 transition-all duration-200 placeholder:font-light focus:outline-1 disabled:opacity-50 ${errors.fullName && "outline-red-500"}`} required />
+                <div className="bg-grey-500/40 block w-0.25 self-stretch max-lg:hidden" />
+                <form className="flex basis-150 flex-col max-lg:basis-auto">
+                    <label className="max-3xl:mb-7 mb-8 max-md:mb-8">
+                        <p className="tracking-6 max-3xl:mb-2.5 mb-3 text-lg/9.5 font-semibold max-2xl:text-base max-md:mb-3">Full Name</p>
+                        <input type="text" name="fullName" value={values.fullName} onChange={handleChange} minLength={2} maxLength={50} disabled={formState.currentState !== "idle"}  placeholder="John Doe" className={`placeholder:text-grey-400 w-full rounded-lg bg-white py-3.5 pl-4 transition-all duration-200 placeholder:font-light focus:outline-1 disabled:opacity-50 max-sm:py-4 max-sm:text-sm  ${errors.fullName && "outline-red-500"}`} required />
                         <AnimatePresence>
 
                             {errors.fullName &&
@@ -95,9 +95,9 @@ const FrequentTraveler = () => {
                             }
                         </AnimatePresence>
                     </label>
-                    <label className="mb-12">
-                        <p className="tracking-6 mb-3 text-lg/9.5 font-semibold">Email</p>
-                        <input type="email" name="email" value={values.email} onChange={handleChange} minLength={5} maxLength={100} disabled={formState.currentState !== "idle"} placeholder="email@example.com" className={`placeholder:text-grey-400 w-full rounded-lg bg-white py-3.5 pl-4 transition-all duration-200 placeholder:font-light focus:outline-1 disabled:opacity-50 ${errors.email && "outline-red"}`} required />
+                    <label className="max-3xl:mb-11.5 mb-12 max-md:mb-16">
+                        <p className="tracking-6 max-3xl:mb-2.5 mb-3 text-lg/9.5 font-semibold max-2xl:text-base max-md:mb-3">Email</p>
+                        <input type="email" name="email" value={values.email} onChange={handleChange} minLength={5} maxLength={100} disabled={formState.currentState !== "idle"} placeholder="email@example.com" className={`placeholder:text-grey-400 w-full rounded-lg bg-white py-3.5 pl-4 transition-all duration-200 not-hover:transition-none placeholder:font-light focus:outline-1 disabled:opacity-50 max-sm:py-4 max-sm:text-sm ${errors.email && "outline-red"}`} required />
                         <AnimatePresence>
 
                             {errors.email && <motion.p className="text-red-500 pt-1 pl-0.5 text-sm"
@@ -108,14 +108,14 @@ const FrequentTraveler = () => {
                             >{errors.email}</motion.p>}
                         </AnimatePresence>
                     </label>
-                    <div className="flex flex-wrap item-center justify-between gap-8">
+                    <div className="flex flex-wrap items-center justify-between gap-x-8 gap-y-8">
                         <label className="text-grey-800 flex cursor-pointer items-center gap-x-1.5">
-                            <button className="flex size-5 cursor-pointer items-center justify-center rounded-xs bg-white p-1 disabled:opacity-50" disabled={formState.currentState !== "idle"}  type="button" onClick={() => setIsChecked(!isChecked)}>
+                            <button className="flex size-5 cursor-pointer items-center justify-center rounded-xs bg-white p-1 disabled:opacity-50 max-xl:size-4.5" disabled={formState.currentState !== "idle"}  type="button" onClick={() => setIsChecked(!isChecked)}>
                                 <Checkmark className={`size-2 transition-all duration-200 ${isChecked ? 'visible size-3 opacity-100' : 'invisible size-2 opacity-0'}`} />
                             </button>
-                            <p className="text-sm tracking-[.03rem]">Agree to receive promotional email updates</p>
+                            <p className="text-sm tracking-[.03rem] max-xl:text-xs">Agree to receive promotional email updates</p>
                         </label>
-                        <button className={` enabled:hover:bg-primary-800 cursor-pointer rounded-[0.625rem] px-8 py-3.5 font-medium text-white transition-all duration-200 disabled:cursor-not-allowed ${buttonStateClasses[formState.currentState]}`} onClick={handleSubmit} disabled={formState.currentState !== "idle"}>
+                        <button className={` not-disabled:hover:bg-primary-800 cursor-pointer rounded-[.625rem] px-8 py-3.5 font-medium text-white transition-all duration-200 disabled:cursor-not-allowed max-xl:py-3 max-xl:text-sm max-xl:font-normal max-lg:py-3.5 max-lg:text-base  ${buttonStateClasses[formState.currentState]}`} onClick={handleSubmit} disabled={formState.currentState !== "idle"}>
                             {formState.currentState === "idle" && "Learn More"}
                             {formState.currentState === "pending" && "Submitting..."}
                             {formState.currentState === "error" && "Submission Failed"}
--- src/components/Steps/Rating.tsx
+++ src/components/Steps/Rating.tsx
@@ -12,15 +12,15 @@ interface RatingProps {
 const Rating = ({className,img,name,rating}:RatingProps) => {
   return (
     <motion.div initial={{opacity:0, y:100,scale:0.95}} 
-    whileInView={{opacity:1,y:0,scale:1}} viewport={{once:true,amount:0}}  transition={{duration:0.5,ease:"easeInOut"}} className={`absolute flex gap-x-2.5 rounded-xl  bg-white/85 py-3.5 pr-5 pl-3  drop-shadow-[0px_4px_30px_rgba(0,0,0,0.12)] backdrop-blur-3xl ${className}`} >
-        <div className="bg-grey-300 flex items-end justify-center overflow-hidden rounded-full ">
-            <img src={img} className="size-15" alt="Graphic of person rating photo" />
+    whileInView={{opacity:1,y:0,scale:1}} viewport={{once:true,amount:0}}  transition={{duration:0.5,ease:"easeInOut"}} className={`absolute flex gap-x-2.5 rounded-xl bg-white/85 py-3.5 pr-5 pl-3 drop-shadow-[0_4px_30px_rgba(0,0,0,0.12)] backdrop-blur-3xl max-xl:py-2.5 max-sm:pr-6 max-sm:pl-2 ${className}`} >
+        <div className="bg-grey-300 max-3xl:size-16 flex size-17 items-end justify-center overflow-hidden rounded-full max-2xl:size-14 max-xl:size-13 max-sm:size-10">
+            <img src={img} className="max-3xl:size-14 size-15 max-2xl:size-13 max-xl:size-12 max-sm:size-9" alt="Graphic of person rating photo" />
         </div>
-        <div className="flex flex-col gap-y-2">
-            <p className="tracking-6 font-semibold">{name}</p>
+        <div className="max-3xl:gap-y-1.5 flex flex-col gap-y-2 max-2xl:gap-y-1">
+            <p className="tracking-6 max-3xl:text-sm font-semibold max-sm:text-xs">{name}</p>
             <div className="flex items-center">
-                <Star className="fill-yellow mr-2 size-6" />
-                <p className="text-grey-600 text-[1.0625rem]">{rating}</p>
+                <Star className="fill-yellow mr-2 size-6 max-sm:mr-1.5 max-sm:size-4" />
+                <p className="text-grey-600 max-3xl:text-[.9375rem] text-[1.0625rem] font-semibold max-xl:text-sm max-sm:text-xs">{rating}</p>
             </div>
         </div>
     </motion.div>
